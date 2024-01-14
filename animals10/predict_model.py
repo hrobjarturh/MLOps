0cd09d114e8193839e99689d378733f6842f90ce
magnusnyholm <132459009+magnusnyholm@users.noreply.github.com>
2024-01-14 23:02:56 +0100

    training model and added cuda to predict_model

--- animals10/predict_model.py
+++ animals10/predict_model.py
@@ -1,19 +1,18 @@
 from typing import List, Union
-
 import torch
 from PIL import Image
-
 from animals10.data.Preprocessing import Preprocessing
 from animals10.models.GoogLeNet import GoogLeNet
 
 
 class Predictor:
-    def __init__(self, model_path="models/googlenet_model.pth"):
+    def __init__(self, model_path="models/googlenet_model0.pth"):
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.model = self.load_model(model_path)
 
     def load_model(self, model_path: str):
         model = GoogLeNet().model
-        model.load_state_dict(torch.load(model_path))
+        model.load_state_dict(torch.load(model_path, map_location=self.device))
         model.eval()
         return model
 
@@ -27,8 +26,8 @@ class Predictor:
 
         for image_path in image_paths:
             input_image = Image.open(image_path).convert("RGB")
-
             input_batch = Preprocessing.preprocess_images(input_image, IMAGE_SIZE)
+            input_batch = input_batch.to(self.device)
 
             with torch.no_grad():
                 output = self.model(input_batch)
@@ -36,6 +35,6 @@ class Predictor:
             probabilities = torch.nn.functional.softmax(output[0], dim=0)
             probabilities, labels = torch.topk(probabilities, top_amount)
 
-            results.append((float(probabilities), int(labels)))
+            results.append((float(probabilities.cpu()), int(labels.cpu())))
 
         return results

a68ae5f21e3b2157271a5281ccd112dc26e61cf8
hrobjarturh <hrobjarturh@gmail.com>
2024-01-16 18:39:01 +0100

    added gcloud cloudbuild

--- animals10/serve.py
+++ animals10/serve.py
@@ -1,4 +1,10 @@
-from fastapi import FastAPI
+import io
+from io import BytesIO
+from typing import List
+
+import torch
+from fastapi import Depends, FastAPI, File, HTTPException, Request, UploadFile
+from PIL import Image
 from pydantic import BaseModel
 
 from animals10.predict_model import Predictor
@@ -8,21 +14,6 @@ app = FastAPI()
 # On macOS, the path to the model is different
 predictor = Predictor("models/googlenet_model_0.pth")
 
-
-class Data(BaseModel):
-    image_path: str | list[str]
-
-
-@app.post("/predict")
-async def predict(data: Data):
-    if isinstance(data.image_path, str):
-        data.image_path = [data.image_path]
-
-    results = predictor.predict(data.image_path)
-
-    return {"result": results}
-
-
 @app.get("/ping")
 def ping():
     return "Pong"
@@ -30,3 +21,23 @@ def ping():
 @app.get("/")
 def welcome():
     return "Status: Live"
+
+class TensorInput(BaseModel):
+    data: List[List[List[List[float]]]]
+
+@app.post("/process_tensor")
+async def process_tensor(tensor_input: TensorInput):
+    # Convert the received data to a torch tensor
+    try:
+        tensor_data = torch.tensor(tensor_input.data)
+        # Check if the tensor has the expected shape
+        if tensor_data.shape != (1, 3, 224, 224):
+            raise ValueError("Invalid tensor shape. Expected torch.Size([1, 3, 224, 224])")
+        
+        results = predictor.predict(tensor_data)
+        
+        return {"results": results}
+
+
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e))
---
+++ dockerfiles/server_model.dockerfile
@@ -0,0 +1,20 @@
+# Use a base image with Python installed
+FROM python:3.11-slim
+
+# 
+WORKDIR /code
+
+# 
+COPY ./requirements_serve.txt /code/requirements_serve.txt
+
+RUN pip install -r /code/requirements_serve.txt --no-cache-dir
+
+
+COPY ./animals10 /code/animals10
+COPY ./models /code/models
+COPY ./models/googlenet_model_0.pth /code/models/googlenet_model_0.pth
+COPY ./data/processed/test /code/data/processed/test
+COPY ./config /code/config
+
+# 
+CMD ["uvicorn", "animals10.serve:app", "--host", "0.0.0.0", "--port", "8080"]

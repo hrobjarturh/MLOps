c619a1b86bc1c69381e3e69dfbb0b9a5d42e8a5c
Jacob Ottensten <jaxot@hotmail.com>
2024-01-18 15:43:13 +0100

    val config edit

--- animals10/Loader.py
+++ animals10/Loader.py
@@ -1,6 +1,7 @@
+import io
+
 import torch
 from google.cloud import storage
-import io
 from torch.utils.data import ConcatDataset, DataLoader
 
 
@@ -19,7 +20,6 @@ class Loader:
         print("Prefix", {prefix})
         print("Bucket", {bucket})
 
-
         # List to store individual datasets
         datasets = []
         batch_counter = 0
@@ -27,7 +27,7 @@ class Loader:
         blobs = client.list_blobs(bucket, prefix=prefix)
 
         for blob in blobs:
-            if blob.name.endswith('.pt'):
+            if blob.name.endswith(".pt"):
                 print(f"Loading {blob.name} from bucket {bucket_name}")
                 batch_counter += 1
                 file_data = blob.download_as_bytes()
@@ -36,7 +36,7 @@ class Loader:
                 dataset = torch.load(io.BytesIO(file_data))
                 datasets.append(dataset)
 
-                if batch_counter >= batch_amount and "train/" in blob.name:
+                if batch_counter >= batch_amount:
                     break
 
         if not datasets:
@@ -46,9 +46,9 @@ class Loader:
             concatenated_dataloader = DataLoader(concatenated_dataset, hyperparams.batch_size, shuffle=True)
             print(f"Finished loading from {gcs_path}")
             return concatenated_dataloader
-        
+
     def parse_gcs_path(self, gcs_path):
         path_parts = gcs_path.replace("gs://", "").split("/")
         bucket_name = path_parts[0]
-        prefix = '/'.join(path_parts[1:])
+        prefix = "/".join(path_parts[1:])
         return bucket_name, prefix
